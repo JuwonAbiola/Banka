40fd24d6e1b4d767ec2a91d0f19f0aec8a809379
oluwajhay <lordsylharx@gmail.com>
2019-04-12 07:51:00 +0100

    chore(test):add first test commit [Start #165299370]

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable import/no-unresolved */
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import version from './version/v1';
 
 const app = express();
 const port = process.env.NODE_ENV || 5000;
@@ -8,6 +10,7 @@ const port = process.env.NODE_ENV || 5000;
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api/v1', version);
 
 app.get('/', (req, res) => res.send({ message: 'Welcome to Banka' }).status(200));
 app.listen(port);
---
+++ SERVER/version/v1.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-unresolved */
+import { Router } from 'express';
+
+const api = Router();
+
+api.get('/', (req, res) => res.send({
+    ok: true,
+    message: 'Welcome to Andela',
+    status: 'API version 1',
+}));
+
+api.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+api.use((req, res) => res.status(404).send({
+    message: 'Sorry that route/method doesnt exist',
+}));
+
+export default api;
