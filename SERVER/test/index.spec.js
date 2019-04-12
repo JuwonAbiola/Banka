9b6c52c67bc2975b97f75a046fd2f681725aa525
oluwajhay <lordsylharx@gmail.com>
2019-04-12 16:03:39 +0100

    chore(test): add test update [#165299370]

--- SERVER/test/index.spec.js
+++ SERVER/test/index.spec.js
@@ -7,7 +7,7 @@ chai.should();
 
 describe('UNIT TESTS DATA CONTROLLERS', () => {
   describe('/POST REQUEST', () => {
-    it('it should POST user sign up', (done) => {
+    it('it should POST user sign up', () => {
       chai
         .request(server)
         .post('/api/v1/auth/signup')
@@ -22,14 +22,29 @@ describe('UNIT TESTS DATA CONTROLLERS', () => {
           res.body.should.have.property('message').to.equals('Registered successfully');
           res.body.should.have.property('status').to.equals(201);
           res.body.should.have.property('data');
-          done();
+        });
+    });
+
+    it('it should fail POST user sign up', () => {
+      chai
+        .request(server)
+        .post('/api/v1/auth/signup')
+        .send({
+          firstName: 'juwon',
+          lastName: 'jhay',
+          email: 'juwon@gmail.com',
+          password: 'password',
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.have.property('error').to.equals('Email already registered');
+          res.body.should.have.property('status').to.equals(400);
         });
     });
   });
 
-
   describe('/POST REQUEST', () => {
-    it('it should POST user login', (done) => {
+    it('it should POST user login', () => {
       chai
         .request(server)
         .post('/api/v1/auth/signin')
@@ -42,7 +57,109 @@ describe('UNIT TESTS DATA CONTROLLERS', () => {
           res.body.should.have.property('message').to.equals('Logged in successfully');
           res.body.should.have.property('status').to.equals(200);
           res.body.should.have.property('data');
-          done();
+        });
+    });
+
+    it('it should fail POST user login', () => {
+      chai
+        .request(server)
+        .post('/api/v1/auth/signin')
+        .send({
+          email: 'blabla@gmail.com',
+          password: 'password',
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.have.property('error').to.equals('Incorrect email');
+          res.body.should.have.property('status').to.equals(400);
+        });
+    });
+  });
+
+  describe('/POST REQUEST', () => {
+    it('it should POST user account', () => {
+      chai
+        .request(server)
+        .post('/api/v1/accounts')
+        .send({
+          email: 'juwon@gmail.com',
+          type: 'current',
+          openingBalance: 32333,
+        })
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.have.property('message').to.equals('Account created successfully');
+          res.body.should.have.property('status').to.equals(201);
+          res.body.should.have.property('data');
+        });
+    });
+
+    it('it should fail POST user account', () => {
+      chai
+        .request(server)
+        .post('/api/v1/accounts')
+        .send({
+          email: 'juwonfew@gmail.com',
+          type: 'current',
+          openingBalance: 32333,
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.have.property('status').to.equals(400);
+          res.body.should.have.property('error').to.equals('Please sign up before creating account');
+        });
+    });
+  });
+
+  describe('/PATCH REQUEST', () => {
+    it('it should PATCH user account', () => {
+      chai
+        .request(server)
+        .patch('/api/v1/accounts/7767637188')
+        .send({
+          status: 'dormant',
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('message').to.equals('Updated successfully');
+          res.body.should.have.property('status').to.equals(200);
+          res.body.should.have.property('data');
+        });
+    });
+
+    it('it should fail PATCH user account', () => {
+      chai
+        .request(server)
+        .patch('/api/v1/accounts/7767637187')
+        .send({
+          status: 'dormant',
+        })
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.have.property('status').to.equals(404);
+          res.body.should.have.property('error').to.equals('Account with the given account number is not found');
+        });
+    });
+  });
+
+  describe('DELETE REQUEST', () => {
+    it('it should DELETE account', () => {
+      chai.request(server)
+        .delete('/api/v1/accounts/7767637188')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.have.property('message').to.equals('Account successfulluy deleted');
+          res.body.should.have.property('status').to.equals(200);
+        });
+    });
+
+    it('it should return error', () => {
+      chai.request(server)
+        .delete('/api/v1/accounts/2345566767')
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.have.property('error').to.equals('Account with the given account number is not found');
+          res.body.should.have.property('status').to.equals(404);
         });
     });
   });
