eb88757939a239da49a4532fe33634181d31d7d9
oluwajhay <lordsylharx@gmail.com>
2019-04-28 12:48:15 +0100

    chore(server):write test for routes based on LFA feedbacks [maintain #165432620]

--- SERVER/index.js
+++ SERVER/index.js
@@ -5,7 +5,8 @@ import version from './version/v1';
 
 const app = express();
 
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 5000;
+console.log(port);
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
