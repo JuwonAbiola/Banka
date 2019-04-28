2795ef1c69c7d2ee525663a0fb92ce69ffee1072
oluwajhay <lordsylharx@gmail.com>
2019-04-28 17:19:45 +0100

    bug(ui):based on LFA feedback seperate login style [fixes #165432620]

--- UI/assets/js/app.js
+++ UI/assets/js/app.js
@@ -1,3 +1,13 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
 function openForm() {
     document.getElementById("myForm").style.display = "block";
 }
